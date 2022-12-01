fc4ef527e8030fd405ec4ea7a6baf16f44303e7d
Coding World <111302606+saeednoor611@users.noreply.github.com>
2022-12-01 09:25:47 -0800

    Home Page With Animation in c++
    Home Page With Animation in c++

---
+++ introduction front page.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+#include<conio.h>
+#include<iomanip>
+#include<windows.h>
+using namespace std;
+
+
+main()
+{
+	cout<<"\n\n\t\t\t========>  HI MUDASIR LOVE YOU  <==========";
+	cout<<"\n\n\t\t\t&&&    LOVE YOU FOR WHAT YOU DID    &&&";
+	cout<<"\n\n\t\t\t,,,,,,,........for palestine.......,,,,,,,";
+	cout<<"\n\n\t\t...................................................";
+	Sleep(500);
+	cout<<"\n\n\t\t...................................................";
+	Sleep(500);
+	cout<<"\n\n\t\t\t\tINTRODUCTION PAGE...";
+	Sleep(500);
+	cout<<"\n\n\t\t...................................................";
+	Sleep(500);
+	cout<<"\n\t\tRoll No. :"<<setw(30)<<"2035";
+	Sleep(500);
+	cout<<"\n\t\tName. :"<<setw(33)<<"Noor Saeed";
+	Sleep(500);
+	cout<<"\n\t\tSection. :"<<setw(31)<<"BSCS.(hons.)";
+	Sleep(500);
+	cout<<"\n\t\tTeacher. :"<<setw(29)<<"Javed";
+	Sleep(500);
+	cout<<"\n\t\tInstitute. :"<<setw(30)<<"DEGREE COLLEGE NO 1";
+	Sleep(500);
+	cout<<"\n\n\t\t...................................................";
+	Sleep(500);
+	cout<<"\n\n\t\t...................................................";
+	Sleep(500);
+	cout<<"\n\n\t\t...................................................";
+	Sleep(500);
+	getch();
+	return 0;
+}
